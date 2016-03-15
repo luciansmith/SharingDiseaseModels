de86b3cd80bfb24723aff9cbf48ba25ff8fa4564
Lucian Smith <lpsmith@uw.edu>
2016-03-15 15:30:58 -0700

    Update Example3.py

--- Example3.py
+++ Example3.py
@@ -29,26 +29,23 @@
 #This implementation uses a flattened model separated to M=Male and F=Female
 
 import tellurium as te
+import matplotlib.pyplot as plt
 
 r = te.loada ('''
 MJ00: MH -> MS; MH*0.2
 MJ02: MH -> MD; MH*0.01
 MJ10: MS -> MH; MS*0.1
 MJ12: MS -> MD; MS*0.3
-
 FJ00: FH -> FS; FH*0.1
 FJ02: FH -> FD; FH*0.01
 FJ10: FS -> FH; FS*0.1
 FJ12: FS -> FD; FS*0.3
-
 MH = 50
 MS = 0
 MD = 0
-
 FH = 50
 FS = 0
 FD = 0
-
 ''')
 
 # This will create the SBML XML file
@@ -56,6 +53,17 @@ te.saveToFile ('Example3.xml', r.getSBML())
 
 r.setIntegrator('gillespie')
 r.integrator.variable_step_size = True
-r.getIntegrator().setValue('seed', 0)
+r.getIntegrator().setValue('seed', 1010)
 result = r.simulate(0,10)
-r.plot (result)
+#r.plot (result)
+
+plt.plot (result[:,0], result[:,1], linestyle='-', linewidth=2)
+plt.plot (result[:,0], result[:,2], linestyle='--', linewidth=2)
+plt.plot (result[:,0], result[:,3], linestyle=':', linewidth=2)
+plt.plot (result[:,0], result[:,4], linestyle='-.', linewidth=2)
+plt.plot (result[:,0], result[:,5], linestyle='-', linewidth=2)
+plt.plot (result[:,0], result[:,6], linestyle='--', linewidth=2)
+plt.xlabel("Time", fontsize="xx-large")
+plt.ylabel("Individuals", fontsize="xx-large")
+plt.legend(['[MH]', '[MS]', '[MD]', '[FH]', '[FS]', '[FD]'])
+plt.show()
